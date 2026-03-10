f0dc84d6bd86ab23f93d83414293570cb1599b43
gangababu M <gangababumedisetty.com>
2026-03-10 17:52:47 +0530

    action

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ resource "aws_route_table" "database" {
 }
 
 resource "aws_route" "public" {
-    route_table_id            =  aws_route_table.private.id
+    route_table_id            =  aws_route_table.public.id
     destination_cidr_block    = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
 }
