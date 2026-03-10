c2ff48ef82a824bc49e6023c2ca04fbdbe350e37
gangababu M <gangababumedisetty.com>
2026-03-10 17:12:44 +0530

    action

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_subnet" "private" {
     count = length(var.private_subnet_cidrs)
     vpc_id = aws_vpc.main.id
     cidr_block = var.private_subnet_cidrs[count.index]
-    availability_zone = local.az_name[count.index]
+    availability_zone = local.az_names[count.index]
 
     tags = merge(
         local.common_tags,
@@ -51,7 +51,7 @@ resource "aws_subnet" "database" {
     count = length(var.database_subnet_cidrs)
     vpc_id = aws_vpc.main.id
     cidr_block = var.database_subnet_cidrs[count.index]
-    availability_zone = local.az_name[count.index]
+    availability_zone = local.az_names[count.index]
 
     tags = merge(
         local.common_tags,
@@ -64,7 +64,7 @@ resource "aws_subnet" "database" {
 }
 
 resource "aws_route_table" "public" {
-    vpc_id = aws.vpc.main.id
+    vpc_id = aws_vpc.main.id
 
     tags = merge(
         local.common_tags,
@@ -78,7 +78,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table" "private" {
-    vpc_id = aws.vpc.main.id
+    vpc_id = aws_vpc.main.id
 
 
     tags = merge(
@@ -92,7 +92,7 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table" "database" {
-    vpc_id = aws.vpc.main.id
+    vpc_id = aws_vpc.main.id
 
 
     tags = merge(
